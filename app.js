ebd83a844e261c70834d786e16f1f5cf4071ace9
mnubhan <nubhant@gmail.com>
2023-02-23 08:24:26 +0000

    feat: parse weather data in JSON format

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ app.get("/", function (req, res) {
     "https://api.openweathermap.org/data/2.5/weather?q=Kuala Lumpur&appid=5400310b05081053d9190cc8348b501f&units=metric";
   https.get(url, function (response) {
     console.log(response);
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      console.log(temp);
+    });
   });
+
   res.send("Server is up and running");
 });
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-// write a function plus
